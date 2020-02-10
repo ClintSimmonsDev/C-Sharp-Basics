9b00533b73e8ac14ef1c65e2bfd2c56830fc47de
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-10 13:58:37 -0800

    Add C# Test Projects

---
+++ C-Sharp/C-SharpTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingWelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+        }
+    }
+}
