90e263a6b37b07da374471a514fdea05c2f46eb6
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-10 20:59:02 -0800

    Upload C# Project Files

---
+++ C-Sharp/MathAndAssignment_Tests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndAssignment_Tests
+{
+    static class MathAndAssignment_Tests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number - I'll multiply it by 50:");
+            int multiplyBy50 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("That's " + multiplyBy50 + "!");
+
+
+        }
+    }
+}
