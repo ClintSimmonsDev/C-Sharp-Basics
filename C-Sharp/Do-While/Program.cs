c157530f4322cc6f5a9b9f8cc6a2e29a2a14af40
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-11 22:25:40 -0800

    Commit Do-While Test

---
+++ C-Sharp/Do-While/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int funkLevel = 0;
+            do
+            {
+                Console.WriteLine("Your current Funk Level is: {0}", funkLevel);
+                funkLevel++;
+                Console.ReadLine();
+                if (funkLevel >= 10)
+                {
+                    Console.WriteLine("You've reached Funk Level 10: Maximum Funk!\n*Confetti*\n");
+                    break;
+                }
+            }
+            while (funkLevel < 10);
+
+        }
+    }
+}
