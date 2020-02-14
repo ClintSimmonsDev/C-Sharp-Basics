c80189c2a8991516dd53cf8beea706eef257477c
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-14 10:11:38 -0800

    Commit MathMethods

---
+++ C-Sharp/MathMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number please, it's math time: \n");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            MathClass Adding = new MathClass();
+            Console.WriteLine(Adding.Add3k(num1));
+
+            MathClass Dividing = new MathClass();
+            Console.WriteLine(Dividing.DivideBy8(num1));
+
+            MathClass Multiplying = new MathClass();
+            Console.WriteLine(Multiplying.MultiplyBy365(num1));
+
+            Console.WriteLine("See what I did there?");
+            Console.ReadLine();
+
+
+        }
+    }
+}
