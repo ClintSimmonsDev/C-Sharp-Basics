6431eb39f5dfa225fd3bcb86251d714b8d119700
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-15 14:57:40 -0800

    Commit VoidAndOverloading

---
+++ C-Sharp/VoidAndOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAndOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass WowFun = new VoidClass();
+            Console.WriteLine("It's that time again - give me a number! ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            WowFun.KoolMethod(input1);
+            ;
+            Console.WriteLine(WowFun.FreshInt(50));
+            Console.WriteLine(WowFun.FreshInt(6.2345678m));
+
+            VoidClass.CoolString();
+        }
+
+    }
+}
---
+++ C-Sharp/VoidAndOverloading/VoidClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAndOverloading
+{
+    public class VoidClass
+    {
+       //Void method
+        public void KoolMethod(int int1)
+        {
+            int1 /= 2;
+            Console.WriteLine(int1);
+        }
+
+        //Overloaded methods
+        public int FreshInt(int sweetInt1, int sweetInt2 = 400)
+        {
+            sweetInt1 *= 2;
+            return sweetInt1 + sweetInt2;
+        }
+
+        public decimal FreshInt(decimal sweetDec1, decimal sweetDec2 = 400m)
+        {
+            sweetDec1 *= 50;
+            return sweetDec1 + sweetDec2;
+        }
+
+        //Static method
+        public static void CoolString()
+        {
+            Console.WriteLine("You done did it");
+        }
+    }
+}
