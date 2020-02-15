f9af252ff039fc62f2e0995044b125fff5277b5b
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-15 12:26:15 -0800

    Commit OptionalArguments

---
+++ C-Sharp/OptionalArguments/MathOps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalArguments
+{
+    public class MathOps
+    {
+        public int MathStuff(int num1, int num2 = 88)
+        {
+            
+            return num1 + num2;
+
+        }
+    }
+}
