18098ef79c0d89eb4d596858532ccc50659eaca1
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-15 18:24:28 -0800

    Commit Abstract Classes

---
+++ C-Sharp/AbstractClasses/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine("Student Name: " + fullName);
+            base.SayName();
+        }
+    }
+}
---
+++ C-Sharp/AbstractClasses/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public virtual void SayName()
+        {
+            string fullName = firstName + " " + lastName;
+        }
+    }
+}
