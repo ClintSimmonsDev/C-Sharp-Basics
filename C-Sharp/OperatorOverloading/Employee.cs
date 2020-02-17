ba7a5ae72ed07a5df13328e96635b7abcb4befc5
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-16 17:34:05 -0800

    Commit OperatorOverloading

---
+++ C-Sharp/OperatorOverloading/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        //Overloader active && Id's the same = true
+        //Overloader NOT active || Id's NOT the same = false
+        public static bool operator ==(Employee Randy, Employee Gertrude)
+        {
+            return Randy.Id.Equals(Gertrude.Id);
+        }
+        public static bool operator !=(Employee Randy, Employee Gertrude)
+        {
+            return !Randy.Id.Equals(Gertrude.Id);
+        }
+    }
+}
