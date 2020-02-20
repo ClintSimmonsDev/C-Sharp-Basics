8316beeb2ac7d87ee42c0f23cfaa39c387f89f9f
Clint Simmons <clintsimmons.dev@gmail.com>
2020-02-20 12:02:11 -0800

    Commit ConstChainAndVar

---
+++ C-Sharp/ConstChainAndVar/Constructors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstChainAndVar
+{
+    public class Constructors
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Constructors(string name) : this(name, 55)
+        {
+
+        }
+        public Constructors(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
